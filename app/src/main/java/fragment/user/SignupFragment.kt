25accd16e7eb5e7ba681d98c326a9b25631d1cec
Alex <alexander.cg.carlsson@gmail.com>
2022-10-21 15:49:39 +0200

    user information is sent to db on create user auth is sent to auth on create

--- app/src/main/java/fragment/user/SignupFragment.kt
+++ app/src/main/java/fragment/user/SignupFragment.kt
@@ -10,6 +10,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import com.example.fooddeliveryproject.R
+import com.example.fooddeliveryproject.User
+import com.example.fooddeliveryproject.db
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -21,7 +23,8 @@ class SignupFragment : Fragment() {
     private lateinit var woopTV1 : TextView
     private lateinit var woopTV2 : TextView
     private lateinit var registerTextView: TextView
-    private lateinit var newUsernameEditText : EditText
+    private lateinit var newUserNameEditText : EditText
+    private lateinit var newUserEmailEditText : EditText
     private lateinit var newUserAddressEditText : EditText
     private lateinit var newUserPhoneNumberEditText : EditText
     private lateinit var newUserPasswordEditText : EditText
@@ -38,9 +41,12 @@ class SignupFragment : Fragment() {
         woopTV2 = v.findViewById(R.id.WoopTV2)
 
         registerTextView = v.findViewById(R.id.registerTextView)
-        newUsernameEditText = v.findViewById(R.id.newUsernameEditText)
+
+        newUserNameEditText = v.findViewById(R.id.newUserNameEditText)
+        newUserEmailEditText = v.findViewById(R.id.newUserEmailEditText)
         newUserAddressEditText = v.findViewById(R.id.newUserAddressEditText)
         newUserPhoneNumberEditText = v.findViewById(R.id.newUserPhoneNumberEditText)
+
         newUserPasswordEditText = v.findViewById(R.id.newUserPasswordEditText)
         newUserSignupButton = v.findViewById(R.id.newUserSignupButton)
 
@@ -51,12 +57,48 @@ class SignupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         newUserSignupButton.setOnClickListener{
-            onClick()
+
+            createUserToDBCatalougeFromSignupField()
+            createAuthUserFromSignupField(newUserEmailEditText.text.toString(), newUserPasswordEditText.text.toString())
         }
 
     }
     private fun onClick() {
         Log.d("!!!","Klick")
+    }
+
+
+
+    private fun createUserToDBCatalougeFromSignupField() {
+
+        val name = newUserNameEditText.text.toString()
+        val email = newUserEmailEditText.text.toString()
+        val address = newUserAddressEditText.text.toString()
+
+
+        if (name.isEmpty() || email.isEmpty() || address.isEmpty() ){
+            Log.d("!!!", "empty fields, no user created")
+        } else {
+
+           val newUser = User(name = name, email = email, address = address)
+           db.collection("users").document("user").set(newUser)
+               .addOnSuccessListener { task ->
+                   Log.d("!!!", "user created to DB")
+               }
+               .addOnFailureListener{
+                   Log.d("!!!", "No user created to DB")
+               }
+        }
+
+
+    }
+
+    private fun createAuthUserFromSignupField(name : String, email : String){
+        auth.createUserWithEmailAndPassword(name, email)
+            .addOnSuccessListener {  Log.d("!!!", "user created to AUTHlogin")
+            } .addOnFailureListener {
+                Log.d("!!!", "No user created to AUTHlogin")
+            }
     }
 
 }
