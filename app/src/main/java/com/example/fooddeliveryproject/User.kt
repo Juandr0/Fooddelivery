d19a9540bdbcf1a2699b5c7274d4e5284e211082
Alex <47304533+Juandr0@users.noreply.github.com>
2022-10-26 15:27:12 +0200

    Update User.kt

--- app/src/main/java/com/example/fooddeliveryproject/User.kt
+++ app/src/main/java/com/example/fooddeliveryproject/User.kt
@@ -3,7 +3,7 @@ package com.example.fooddeliveryproject
 data class User (var name : String? = null,
                  var email : String? = null,
                  var address : String? = null,
-                 var phoneNumber : String? = null,
+                 var phoneNumber : Int? = null,
                  var type : String? = "user",
                  var uID : String? = null,
                  var lastOrderRestaurant : String? = "Restaurant",
