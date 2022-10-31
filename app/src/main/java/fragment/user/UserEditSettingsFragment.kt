30ae5a63dc57dbafda4dc197d3c9532fd1d61c85
Alex <alexander.cg.carlsson@gmail.com>
2022-10-31 11:30:46 +0100

    added empty field error on editpage

--- app/src/main/java/fragment/user/UserEditSettingsFragment.kt
+++ app/src/main/java/fragment/user/UserEditSettingsFragment.kt
@@ -1,5 +1,6 @@
 package fragment.user
 
+import android.graphics.Color
 import android.os.Bundle
 import android.text.InputType.TYPE_CLASS_NUMBER
 import android.text.InputType.TYPE_CLASS_TEXT
@@ -11,8 +12,10 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.example.fooddeliveryproject.R
 import com.example.fooddeliveryproject.db
+import java.util.*
 
 class UserEditSettingsFragment : Fragment() {
     private lateinit var userAttributeToChangeTextView : TextView
@@ -36,19 +39,44 @@ class UserEditSettingsFragment : Fragment() {
 
         val settingToChange = initializeArgumentSettingToChange()
         val userSetting = initializeArgumentUserSetting()
+        when (settingToChange){
+            "Name" -> userAttributeToChangeEditText.hint = getString(R.string.enter_name)
+            "Address" -> userAttributeToChangeEditText.hint = getString(R.string.enter_address)
+            "Phone number" -> userAttributeToChangeEditText.hint = getString(R.string.enter_phonenr)
+        }
         initializeLayout(settingToChange, userSetting)
 
         //Onclick: Takes the text in edittext and sends it to fun updateUserInDb
         saveSettingButton.setOnClickListener {
-            val currentUserId = auth.currentUser!!.uid
-            val newSetting = userAttributeToChangeEditText.text.toString()
-            updateUserInDb(currentUserId, settingToChange, newSetting)
-            setCurrentFragmentToProfile()
+
+            val emptyCheck = isFieldEmpty()
+
+            if (!emptyCheck){
+                val currentUserId = auth.currentUser!!.uid
+                val newSetting = userAttributeToChangeEditText.text.toString()
+                updateUserInDb(currentUserId, settingToChange, newSetting)
+                setCurrentFragmentToProfile()
+            } else {
+                var redColor = "#FFB0B0"
+                userAttributeToChangeEditText.setBackgroundColor(Color.parseColor(redColor))
+                Toast.makeText(context, getString(R.string.no_empty_fields, userAttributeToChangeTextView.text.toString().toLowerCase()),Toast.LENGTH_SHORT).show()
+
+            }
+
+
         }
+
         goBackButton.setOnClickListener {
             setCurrentFragmentToProfile()
         }
+
+
     }
+
+    private fun isFieldEmpty() : Boolean{
+        return userAttributeToChangeEditText.text.isEmpty()
+    }
+
 
     //Initialize the layout by filling the textview and edittext
     //If the setting to be changed is a phone number, only digits will be displayed -
