d335cb6a6834c7669bd278cd2c506f4c15564e9c
Alex <alexander.cg.carlsson@gmail.com>
2022-11-08 12:47:26 +0100

    layout changes

--- app/src/main/java/fragment/user/OrderConfirmationFragment.kt
+++ app/src/main/java/fragment/user/OrderConfirmationFragment.kt
@@ -1,16 +1,20 @@
 package fragment.user
 
+import adapters.OrderConfirmationRecyclerAdapter
+import adapters.OrderRecyclerAdapter
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import classes.ShoppingCart
 import com.example.fooddeliveryproject.R
 
 class OrderConfirmationFragment : Fragment() {
-
+    var itemsList = mutableListOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,16 +26,29 @@ class OrderConfirmationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val args = this.arguments
 
+        getBundleInfo()
+        initalizeRecyclerView(view)
+        var totalPrice = ShoppingCart.calculateTotalPrice()
+    }
+
+
+    //Gets bundle info of order items and adds it to the itemlist
+    private fun getBundleInfo(){
+        val args = this.arguments
         val lastOrder = args?.getStringArrayList("bundleList")
         if (lastOrder != null) {
             for (item in lastOrder){
-                Log.d("!!!",item)
+                itemsList.add(item)
             }
         }
+    }
 
-        var totalPrice = ShoppingCart.calculateTotalPrice()
+    private fun initalizeRecyclerView(view : View){
+        val recyclerView = view.findViewById<RecyclerView>(R.id.confirmation_orderRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        val adapter = OrderConfirmationRecyclerAdapter(OrderConfirmationFragment(), itemsList)
+        recyclerView.adapter = adapter
     }
 
 }
