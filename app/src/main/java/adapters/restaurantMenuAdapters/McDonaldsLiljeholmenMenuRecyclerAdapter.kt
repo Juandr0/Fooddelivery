76c168076dd53f1c5c4f3ec4908c3aa114777bc2
Anubisto <47527905+Anubisto@users.noreply.github.com>
2022-11-09 20:48:54 +0100

    Added McDonalds menu fragment and path. Added path to almost all restaurants in burger category

---
+++ app/src/main/java/adapters/restaurantMenuAdapters/McDonaldsLiljeholmenMenuRecyclerAdapter.kt
@@ -0,0 +1,100 @@
+package adapters.restaurantMenuAdapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import classes.OrderItem
+import classes.ShoppingCart
+import com.example.fooddeliveryproject.R
+import fragment.restaurantMenu.McDonaldsLiljeholmenMenuFragment
+
+class McDonaldsLiljeholmenMenuRecyclerAdapter (val context: McDonaldsLiljeholmenMenuFragment, val orderItems: List<OrderItem>) :
+    RecyclerView.Adapter<McDonaldsLiljeholmenMenuRecyclerAdapter.ViewHolder>() {
+
+
+    //onClickListener setup
+    private lateinit var mListener: onItemClickListener
+
+    interface onItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListener) {
+        mListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.restaurant_menu_list_item, parent, false)
+        return ViewHolder(itemView, mListener)
+
+
+    }//End of clickListener
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //Takes the right restaurant from the list
+        val orderItem = orderItems[position]
+//        val orderRestaurantName = orderItem.restaurantName
+//        val orderDeliveryFee = orderItem.deliveryFee
+        //Assigns the right information to the restaurant
+        holder.orderItemNameView.text = orderItem.orderFromMeny
+        holder.orderItemNPriceView.text = "${orderItem.price} kr"
+
+        holder.orderRestaurantName = orderItem.restaurantName
+        holder.orderDeliveryFee = orderItem.deliveryFee
+        holder.orderItemPrice = orderItem.price
+        holder.orderItemName = orderItem.orderFromMeny
+
+
+
+
+//        //Glide for image
+//        Glide.with(context)
+//            .load(restaurant.image)
+//            .into(holder.restaurantHeaderImageView)
+
+    }
+
+    //counts how many restaurants there are in the list
+    override fun getItemCount(): Int {
+        return orderItems.size
+    }
+
+
+    inner class ViewHolder(itemView: View, listener: onItemClickListener) :
+        RecyclerView.ViewHolder(itemView) {
+        //When a viewHolder is created, it finds the ImageView and textViews in our itemView
+        var orderItemNameView = itemView.findViewById<TextView>(R.id.orderItemNameView)
+        var orderItemNPriceView = itemView.findViewById<TextView>(R.id.orderItemPriceView)
+
+        var orderRestaurantName = ""
+        var orderDeliveryFee = 0
+        var orderItemPrice = 0
+        var orderItemName = ""
+
+        var orderButton = itemView.findViewById<Button>(R.id.addToOrderButton).setOnClickListener{
+            val orderRestaurant = orderRestaurantName
+            val orderMenuItem = orderItemName
+            val orderPrice = orderItemPrice
+            val deliveryFee = orderDeliveryFee
+            val newOrder = OrderItem(orderRestaurant, orderMenuItem, orderPrice, deliveryFee)
+            ShoppingCart.addItemToCart(newOrder)
+        }
+
+        //init of the clicklistener that checks position
+        init {
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+
+            }
+        }
+
+
+
+    }
+
+}
--- app/src/main/java/fragment/restaurantMenu/McDonaldsLiljeholmenMenuFragment.kt
+++ app/src/main/java/fragment/restaurantMenu/McDonaldsLiljeholmenMenuFragment.kt
@@ -1,7 +1,6 @@
-package fragment.user
+package fragment.restaurantMenu
 
-import adapters.CategoryHamburgersRecyclerAdapter
-import android.content.Intent
+import adapters.restaurantMenuAdapters.McDonaldsLiljeholmenMenuRecyclerAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,14 +9,9 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import classes.Restaurant
+import classes.OrderItem
 import com.example.fooddeliveryproject.R
-import com.example.fooddeliveryproject.UserInterfaceActivity
 import com.google.firebase.firestore.FirebaseFirestore
-import fragment.restaurantMenu.BrodernasMenuFragment
-import fragment.restaurantMenu.LiljeholmensGrillMenuFragment
-import fragment.restaurantMenu.MaxLiljeholmenMenuFragment
-import fragment.restaurantMenu.McDonaldsLiljeholmenMenuFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,18 +20,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CategoryHamburgersFragment.newInstance] factory method to
+ * Use the [McDonaldsLiljeholmenMenuFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CategoryHamburgersFragment : Fragment() {
+class McDonaldsLiljeholmenMenuFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    private val MaxLiljeholmenMenuFragment= MaxLiljeholmenMenuFragment()
-    private val LiljeholmensGrillMenuFragment = LiljeholmensGrillMenuFragment()
-    private val McDonaldsLiljeholmenMenuFragment = McDonaldsLiljeholmenMenuFragment()
-    private val BrodernasMenuFragment = BrodernasMenuFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,30 +41,29 @@ class CategoryHamburgersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category_hamburgers, container, false)
+        return inflater.inflate(R.layout.fragment_mc_donalds_liljeholmen_menu, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        FirebaseFirestore.getInstance().collection("restaurants")
-            .whereArrayContains("category", "hamburgers")
+        FirebaseFirestore.getInstance().collection("restaurants").document("MYTJUx2kyeSyfHjwUy5n").collection("menu")
+            .whereArrayContains("category", "menu")
             .get()
             .addOnSuccessListener { documents ->
                 for(document in documents){
-                    val restaurant = documents.toObjects(Restaurant::class.java)
+                    val orderItem = documents.toObjects(OrderItem::class.java)
                     //Code for recyclerView
-                    var recyclerView = view.findViewById<RecyclerView>(R.id.hamburgersRecyclerView)
+                    var recyclerView = view.findViewById<RecyclerView>(R.id.mcDonaldsLiljeholmenMenuItemsRecyclerView)
                     //What type of layout the list will have. This makes it a linear list
                     recyclerView.layoutManager = LinearLayoutManager(context)
                     // Created an adapter from our adapter-class and sent in the list of restaurants
-                    val adapter = CategoryHamburgersRecyclerAdapter(this, restaurant)
+                    val adapter = McDonaldsLiljeholmenMenuRecyclerAdapter(this, orderItem)
                     //Connect our adapter to our recyclerView
                     recyclerView.adapter = adapter
                     //End of recyclerView
 
-                    val intent = Intent(context, UserInterfaceActivity::class.java)
-                    adapter.setOnItemClickListener(object : CategoryHamburgersRecyclerAdapter.onItemClickListener {
+                    adapter.setOnItemClickListener(object : McDonaldsLiljeholmenMenuRecyclerAdapter.onItemClickListener {
                         override fun onItemClick(position: Int) {
                             //toast to check if clicking works
                             Toast.makeText(context,
@@ -85,19 +73,13 @@ class CategoryHamburgersFragment : Fragment() {
 
                             when (position) {
                                 0 -> {
-                                    setCurrentFragment(LiljeholmensGrillMenuFragment)
+
+
                                 }
                                 1 -> {
-                                    setCurrentFragment(McDonaldsLiljeholmenMenuFragment)
                                 }
                                 2 -> {
 
-                                }
-                                3 -> {
-                                    setCurrentFragment(MaxLiljeholmenMenuFragment)
-                                }
-                                4 -> {
-                                    setCurrentFragment(BrodernasMenuFragment)
                                 }
                             }
                         }
@@ -113,6 +95,7 @@ class CategoryHamburgersFragment : Fragment() {
             }
     }
 
+
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -120,24 +103,16 @@ class CategoryHamburgersFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoryHamburgersFragment.
+         * @return A new instance of fragment McDonaldsLiljeholmenMenuFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CategoryHamburgersFragment().apply {
+            McDonaldsLiljeholmenMenuFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
-
-    private fun setCurrentFragment(fragment : Fragment){
-
-        val fragmentManager = parentFragmentManager
-        val transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment)
-        transaction.commit()
     }
 }
