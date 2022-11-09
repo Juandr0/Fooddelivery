90ef2be3ad5dd40a2a16b08074ef660e0b0a3caa
Anubisto <47527905+Anubisto@users.noreply.github.com>
2022-11-09 21:22:51 +0100

    Added taco bar menu fragment. Finished with all restaurants and paths

--- app/src/main/java/fragment/restaurantMenu/TacoBarMenuFragment.kt
+++ app/src/main/java/fragment/restaurantMenu/TacoBarMenuFragment.kt
@@ -1,6 +1,6 @@
-package fragment.user
+package fragment.restaurantMenu
 
-import adapters.CategoryMexicanRecyclerAdapter
+import adapters.restaurantMenuAdapters.TacoBarMenuRecyclerAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +9,9 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import classes.Restaurant
+import classes.OrderItem
 import com.example.fooddeliveryproject.R
 import com.google.firebase.firestore.FirebaseFirestore
-import fragment.restaurantMenu.TacoBarMenuFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,15 +20,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CategoryMexicanFragment.newInstance] factory method to
+ * Use the [TacoBarMenuFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CategoryMexicanFragment : Fragment() {
+class TacoBarMenuFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    private val TacoBarMenuFragment = TacoBarMenuFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,31 +41,29 @@ class CategoryMexicanFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category_mexican, container, false)
+        return inflater.inflate(R.layout.fragment_taco_bar_menu, container, false)
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        FirebaseFirestore.getInstance().collection("restaurants")
-            .whereArrayContains("category", "mexican")
+        FirebaseFirestore.getInstance().collection("restaurants").document("sxPm5xMT8dQYuBT7L78u").collection("menu")
+            .whereArrayContains("category", "taco")
             .get()
             .addOnSuccessListener { documents ->
                 for(document in documents){
-                    val restaurant = documents.toObjects(Restaurant::class.java)
+                    val orderItem = documents.toObjects(OrderItem::class.java)
                     //Code for recyclerView
-                    var recyclerView = view.findViewById<RecyclerView>(R.id.mexicanRecyclerView)
+                    var recyclerView = view.findViewById<RecyclerView>(R.id.tacoBarMenuItemsRecyclerView)
                     //What type of layout the list will have. This makes it a linear list
                     recyclerView.layoutManager = LinearLayoutManager(context)
                     // Created an adapter from our adapter-class and sent in the list of restaurants
-                    val adapter = CategoryMexicanRecyclerAdapter(this, restaurant)
+                    val adapter = TacoBarMenuRecyclerAdapter(this, orderItem)
                     //Connect our adapter to our recyclerView
                     recyclerView.adapter = adapter
                     //End of recyclerView
 
-
-                    adapter.setOnItemClickListener(object : CategoryMexicanRecyclerAdapter.onItemClickListener {
+                    adapter.setOnItemClickListener(object : TacoBarMenuRecyclerAdapter.onItemClickListener {
                         override fun onItemClick(position: Int) {
                             //toast to check if clicking works
                             Toast.makeText(context,
@@ -78,7 +73,7 @@ class CategoryMexicanFragment : Fragment() {
 
                             when (position) {
                                 0 -> {
-                                    setCurrentFragment(TacoBarMenuFragment)
+
                                 }
                                 1 -> {
                                 }
@@ -106,24 +101,16 @@ class CategoryMexicanFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoryMexicanFragment.
+         * @return A new instance of fragment TacoBarMenuFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CategoryMexicanFragment().apply {
+            TacoBarMenuFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
-
-    private fun setCurrentFragment(fragment : Fragment){
-
-        val fragmentManager = parentFragmentManager
-        val transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment)
-        transaction.commit()
     }
 }
